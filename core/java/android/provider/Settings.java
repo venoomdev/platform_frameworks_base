81dc26f65ac205cf472f909f34bba3cb4277b7f8
Ido Ben-Hur <idoybh2@gmail.com>
2020-10-09 15:16:28 +0300

    base: Allow hiding notification headers [1/2]
    Gimme back my space, thank you
    
    Change-Id: I5f63a384d2bd2052d4a8a9d49aff1456d743367f
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6089,6 +6089,13 @@ public final class Settings {
         public static final String NOTIFICATION_PULSE_DURATION = "ambient_notification_light_duration";
 
         /**
+         * Controls whether to show notification headers
+         * @hide
+         */
+        @Readable
+        public static final String NOTIFICATION_HEADERS = "notification_headers";
+
+        /**
          * Whether StatusBar icons should use app icon
          * @hide
          */

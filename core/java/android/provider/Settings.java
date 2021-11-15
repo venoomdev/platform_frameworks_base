6eb82a3f73377539fec791774107ec4be0403479
spkal01 <kalligeross@gmail.com>
2021-11-15 12:11:42 +0800

    base: Make colored Statusbar Icons optional [1/2]
    DerpFest edit: disable by default
    
    Change-Id: Ifbd60e68ef518f940da0ff6f5b25c90327351afb
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5951,6 +5951,12 @@ public final class Settings {
 
 
         /**
+         * Whether StatusBar icons should use app icon
+         * @hide
+         */
+        public static final String STATUSBAR_ICONS_STYLE = "statusbar_icons_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -6081,6 +6087,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STATUSBAR_CLOCK_DATE_POSITION);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_VIEW_LOCATION);
             PRIVATE_SETTINGS.add(POCKET_JUDGE);
+            PRIVATE_SETTINGS.add(STATUSBAR_ICONS_STYLE);
         }
 
         /**

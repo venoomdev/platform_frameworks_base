44e249fe4fdf9bc52bf4d175d506e2ee284380dd
SKULSHADY <anushekprasal@gmail.com>
2021-11-25 21:07:00 +0700

    SystemUI: Add toggle to disable battery estimates in QS [1/2]
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Change-Id: I0969aeaf41960c8c0693bd6701ed84d1ea023fd2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5929,6 +5929,13 @@ public final class Settings {
         public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
 
         /**
+         * Whether to show battery estimates in QS
+         * @hide
+         */
+        public static final String QS_SHOW_BATTERY_ESTIMATE = "qs_show_battery_estimate";
+
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
